1e5db34b242c08362d922e66e2088523b60b4f4c
antonzenkov91 <antonzenkov91@gmail.com>
2025-04-15 02:32:05 -0700

    add the design of network statistics

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "../../components/Hero";
 import Futures from "../../components/Futures";
 import FameCoin from "../../components/FameCoin";
+import NetworkStatistics from "../../components/NetworkStatistics";
 
 const Home = () => {
   return (
     <div className="pt-20">
       <Hero />
+      <NetworkStatistics />
       <Futures />
       <FameCoin />
     </div>
