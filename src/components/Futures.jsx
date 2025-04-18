2f043d7bcd35722b0fd461b4b042026c6e595661
antonzenkov91 <antonzenkov91@gmail.com>
2025-04-18 03:11:17 -0700

    add the login and signup popup functionality for discover now button in home page

--- src/components/Futures.jsx
+++ src/components/Futures.jsx
@@ -1,69 +1,105 @@
 import womenScafold from "../assets/images/women-scafold.png";
 import menSinging from "../assets/images/men-singing.png";
 import raphaelRenter from "../assets/images/raphael-renter.png";
-
+import { useState } from "react";
+import LoginSignupModal from "./LoginSignupModal";
 const Futures = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+  const handleScroll = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      const yOffset = -80;
+      const y =
+        element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+
+      window.scrollTo({
+        top: y,
+        behavior: "smooth",
+      });
+    }
+  };
+  // for login modals
+  const openLoginModal = () => {
+    setIsLoginModalOpen(true);
+    setIsOpen(false);
+  };
+
+  const closeLoginModal = () => {
+    setIsLoginModalOpen(false);
+  };
   return (
-    <section
-      id="futured"
-      className="relative text-white pt-12 px-4 md:px-10 lg:px-20"
-      style={{
-        background: "linear-gradient(180deg, #0B0B0B 0%, #1a1a1a 100%)",
-      }}
-    >
-      {/* Fixed Bubbles */}
-      <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0 pointer-events-none">
-        <div className="absolute w-60 h-60 bg-white/5 rounded-full top-[-100px] left-[-100px]" />
-        <div className="absolute w-40 h-40 bg-white/5 rounded-full bottom-[-50px] right-[-50px]" />
-      </div>
+    <>
+      <section
+        id="futured"
+        className="relative text-white pt-12 px-4 md:px-10 lg:px-20"
+        style={{
+          background: "linear-gradient(180deg, #0B0B0B 0%, #1a1a1a 100%)",
+        }}
+      >
+        {/* Fixed Bubbles */}
+        <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0 pointer-events-none">
+          <div className="absolute w-60 h-60 bg-white/5 rounded-full top-[-100px] left-[-100px]" />
+          <div className="absolute w-40 h-40 bg-white/5 rounded-full bottom-[-50px] right-[-50px]" />
+        </div>
 
-      {/* Content Layer */}
-      <div className="relative z-10 text-center container max-w-4xl mx-auto">
-        <h2 className="text-p2 py-5 sm:text-p1 md:text-h6 lg:text-h5 xl:text-h4 2xl:text-h3 lg:text-5xl font-bold text-center text-white">
-          FUTURES
+        {/* Content Layer */}
+        <div className="relative z-10 text-center container max-w-4xl mx-auto">
+          <h2 className="text-p2 py-5 sm:text-p1 md:text-h6 lg:text-h5 xl:text-h4 2xl:text-h3 lg:text-5xl font-bold text-center text-white">
+            FUTURES
+          </h2>
+          <p className="text-grayLight font-medium text-p3">
+            Haven’t Been Able To Qualify? Welcome, You Are Now An Integral Part
+            Of Our Platform.
+            <br />
+            <span className="text-yellow-400 font-semibold">
+              Register To Be Seen And Heard!
+            </span>{" "}
+            You Are A Rising Star
+          </p>
+        </div>
+
+        {/* Images */}
+        <div className="relative z-10 mt-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+          <img
+            src={raphaelRenter}
+            alt="Performer 1"
+            className="w-full h-[600px] object-cover object-center"
+          />
+          <img
+            src={womenScafold}
+            alt="Performer 2"
+            className="w-full h-[600px] object-cover object-center"
+          />
+          <img
+            src={menSinging}
+            alt="Performer 3"
+            className="w-full h-[600px] object-cover object-center"
+          />
+        </div>
+
+        {/* Button */}
+        <div className="relative z-10 flex justify-center mt-10 pb-12">
+          <button
+            onClick={openLoginModal}
+            className="bg-lightYellow cursor-pointer hover:scale-105 text-black font-medium px-6 py-3 rounded-md transition-all duration-300 relative group text-p5 2xl:text-p1"
+          >
+            Discover More
+          </button>
+        </div>
+
+        {/* Import Famecoin Heading */}
+        <h2 className="relative z-10 text-p2 pb-12 sm:text-p1 md:text-h6 lg:text-h5 xl:text-h4 2xl:text-h3 lg:text-5xl font-bold text-center text-primary">
+          <span className="text-white">Import Your</span> Famecoin
         </h2>
-        <p className="text-grayLight font-medium text-p3">
-          Haven’t Been Able To Qualify? Welcome, You Are Now An Integral Part Of
-          Our Platform.
-          <br />
-          <span className="text-yellow-400 font-semibold">
-            Register To Be Seen And Heard!
-          </span>{" "}
-          You Are A Rising Star
-        </p>
-      </div>
-
-      {/* Images */}
-      <div className="relative z-10 mt-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-        <img
-          src={raphaelRenter}
-          alt="Performer 1"
-          className="w-full h-[600px] object-cover object-center"
-        />
-        <img
-          src={womenScafold}
-          alt="Performer 2"
-          className="w-full h-[600px] object-cover object-center"
-        />
-        <img
-          src={menSinging}
-          alt="Performer 3"
-          className="w-full h-[600px] object-cover object-center"
-        />
-      </div>
-
-      {/* Button */}
-      <div className="relative z-10 flex justify-center mt-10 pb-12">
-        <button className="bg-lightYellow cursor-pointer hover:scale-105 text-black font-medium px-6 py-3 rounded-md transition-all duration-300 relative group text-p5 2xl:text-p1">
-          Discover More
-        </button>
-      </div>
-
-      {/* Import Famecoin Heading */}
-      <h2 className="relative z-10 text-p2 pb-12 sm:text-p1 md:text-h6 lg:text-h5 xl:text-h4 2xl:text-h3 lg:text-5xl font-bold text-center text-primary">
-        <span className="text-white">Import Your</span> Famecoin
-      </h2>
-    </section>
+      </section>{" "}
+      {/* Login Modal */}
+      <LoginSignupModal
+        isOpen={isLoginModalOpen}
+        setIsOpenLoginSignup={setIsLoginModalOpen}
+        onClose={closeLoginModal}
+      />
+    </>
   );
 };
 
---
+++ src/components/LoginSignupModal.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { FaTimes } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import LoginModal from "./LoginModal";
+import SignupModal from "./SignupModal";
+const LoginSignupModal = ({ isOpen, onClose, setIsOpenLoginSignup }) => {
+  const modalVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+    exit: { opacity: 0 },
+  };
+
+  const contentVariants = {
+    hidden: { y: -50, opacity: 0 },
+    visible: { y: 0, opacity: 1, transition: { delay: 0.1 } },
+    exit: { y: 50, opacity: 0 },
+  };
+
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+  const [isSignupModalOpen, setIsSignupModalOpen] = useState(false);
+  const [isLoginSignupOpen, setIsLoginSignupOpen] = useState(false);
+
+  // for login modals
+  const openLoginModal = () => {
+    setIsLoginModalOpen(true);
+    setIsOpenLoginSignup(false);
+  };
+
+  const closeLoginModal = () => {
+    setIsLoginModalOpen(false);
+  };
+  // for signup modals
+  const openSignupModal = () => {
+    setIsSignupModalOpen(true);
+    setIsLoginSignupOpen(false);
+    setIsOpenLoginSignup(false);
+  };
+
+  const closeSignupModal = () => {
+    setIsSignupModalOpen(false);
+  };
+  const closeLoginSignupModal = () => {
+    setIsLoginSignupOpen(false);
+  };
+  return (
+    <>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            className="fixed top-0 left-0 w-full h-full z-50 bg-transparent backdrop-blur-sm flex justify-center items-center"
+            variants={modalVariants}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            onClick={onClose}
+          >
+            <motion.div
+              className="bg-black rounded-md p-16 max-w-[500px] text-white relative flex flex-col justify-center items-center" // Modal background
+              variants={contentVariants}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+              onClick={(e) => e.stopPropagation()}
+            >
+              <button
+                onClick={onClose}
+                className="absolute top-2 right-2 text-gray-400 hover:text-white focus:outline-none"
+              >
+                <FaTimes size={20} />
+              </button>
+              <h2 className="text-xl font-semibold mb-4 text-primary">
+                Please Sign Up Or Log In To Trade Any Talent Tokens
+              </h2>
+              <button
+                onClick={openLoginModal}
+                className="bg-gray cursor-pointer hover:bg-primary hover:scale-105 text-white font-medium py-3 px-6 w-full mb-3  rounded-md transition-all duration-300 text-p5  2xl:text-p1"
+              >
+                Log In
+              </button>
+              <button
+                onClick={openSignupModal}
+                className="bg-gray cursor-pointer hover:bg-primary hover:scale-105 text-white font-medium py-3 px-6 w-full mb-3  rounded-md transition-all duration-300 text-p5  2xl:text-p1"
+              >
+                Sign Up
+              </button>
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+      <LoginModal isOpen={isLoginModalOpen} onClose={closeLoginModal} />
+      <SignupModal isOpen={isSignupModalOpen} onClose={closeSignupModal} />
+    </>
+  );
+};
+
+export default LoginSignupModal;
