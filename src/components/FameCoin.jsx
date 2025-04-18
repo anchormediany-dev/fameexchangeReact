4bed19629eb7209fdedf9f946dbd4c33987aa084
antonzenkov91 <antonzenkov91@gmail.com>
2025-04-18 03:30:36 -0700

    add the popup functionality in home pages

--- src/components/FameCoin.jsx
+++ src/components/FameCoin.jsx
@@ -1,20 +1,44 @@
 import React from "react";
 import FameCoinLogo from "../assets/icons/famecoin-logo.svg?react";
+import { useState } from "react";
+import LoginSignupModal from "./LoginSignupModal";
 const FameCoin = () => {
-  return (
-    <section className="famecoin-bg text-white relative min-h-[80vh] flex items-center justify-center">
-      <div className="absolute inset-0 bg-[#070707]/50 z-0" />
-      <div className="container relative z-10 flex flex-col items-center text-center space-y-8 py-12">
-        <h1 className="text-p2 sm:text-p1 md:text-h6 lg:text-h5 xl:text-h4 2xl:text-h3 lg:text-5xl font-bold text-primary">
-          The Platform For FANS To Exchange Famecoin
-        </h1>
-        <FameCoinLogo className="max-w-[424px] h-full" />
+  const [isOpen, setIsOpen] = useState(false);
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+  // for login modals
+  const openLoginModal = () => {
+    setIsLoginModalOpen(true);
+    setIsOpen(false);
+  };
 
-        <button className="bg-lightYellow cursor-pointer hover:scale-105 text-black font-medium px-6 py-3 rounded-md transition-all duration-300  relative group text-p5  2xl:text-p1">
-          Transfer Your Fame Coin To The Platform Now
-        </button>
-      </div>
-    </section>
+  const closeLoginModal = () => {
+    setIsLoginModalOpen(false);
+  };
+  return (
+    <>
+      <section className="famecoin-bg text-white relative min-h-[80vh] flex items-center justify-center">
+        <div className="absolute inset-0 bg-[#070707]/50 z-0" />
+        <div className="container relative z-10 flex flex-col items-center text-center space-y-8 py-12">
+          <h1 className="text-p2 sm:text-p1 md:text-h6 lg:text-h5 xl:text-h4 2xl:text-h3 lg:text-5xl font-bold text-primary">
+            The Platform For FANS To Exchange Famecoin
+          </h1>
+          <FameCoinLogo className="max-w-[424px] h-full" />
+
+          <button
+            onClick={openLoginModal}
+            className="bg-lightYellow cursor-pointer hover:scale-105 text-black font-medium px-6 py-3 rounded-md transition-all duration-300  relative group text-p5  2xl:text-p1"
+          >
+            Transfer Your Fame Coin To The Platform Now
+          </button>
+        </div>
+      </section>
+      {/* Login Modal */}
+      <LoginSignupModal
+        isOpen={isLoginModalOpen}
+        setIsOpenLoginSignup={setIsLoginModalOpen}
+        onClose={closeLoginModal}
+      />
+    </>
   );
 };
 
--- src/components/Process.jsx
+++ src/components/Process.jsx
@@ -1,29 +1,53 @@
 import React from "react";
 import processImg from "../assets/images/process-img.png";
+import { useState } from "react";
+import LoginSignupModal from "./LoginSignupModal";
 const Process = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+  // for login modals
+  const openLoginModal = () => {
+    setIsLoginModalOpen(true);
+    setIsOpen(false);
+  };
+
+  const closeLoginModal = () => {
+    setIsLoginModalOpen(false);
+  };
   return (
-    <section className="process-bg text-white relative min-h-[80vh] flex items-center justify-center">
-      <div className="absolute inset-0 bg-[#070707]/85 z-0" />
-      <div className="container relative z-10 py-12">
-        <div className="md:pb-[120px] pb-12">
-          {" "}
-          <h1 className="text-p2 sm:text-p1 md:text-h6 lg:text-h5 xl:text-h4 2xl:text-h3 lg:text-5xl font-bold text-center text-white">
-            The Process
-          </h1>
-          <p className="text-center font-medium text-p3 mt-8 text-primary">
-            Reverse this process to convers your Famecoin into Fiat Currency
-          </p>
-        </div>
-        <div>
-          <img src={processImg} alt="" />
+    <>
+      {" "}
+      <section className="process-bg text-white relative min-h-[80vh] flex items-center justify-center">
+        <div className="absolute inset-0 bg-[#070707]/85 z-0" />
+        <div className="container relative z-10 py-12">
+          <div className="md:pb-[120px] pb-12">
+            {" "}
+            <h1 className="text-p2 sm:text-p1 md:text-h6 lg:text-h5 xl:text-h4 2xl:text-h3 lg:text-5xl font-bold text-center text-white">
+              The Process
+            </h1>
+            <p className="text-center font-medium text-p3 mt-8 text-primary">
+              Reverse this process to convers your Famecoin into Fiat Currency
+            </p>
+          </div>
+          <div>
+            <img src={processImg} alt="" />
+          </div>{" "}
+          <div className="flex justify-center pt-[115px]">
+            <button
+              onClick={openLoginModal}
+              className="bg-lightYellow cursor-pointer hover:scale-105 text-black font-medium px-6 py-3 rounded-md transition-all duration-300  relative group text-p5  2xl:text-p1"
+            >
+              Discover More
+            </button>
+          </div>
         </div>{" "}
-        <div className="flex justify-center pt-[115px]">
-          <button className="bg-lightYellow cursor-pointer hover:scale-105 text-black font-medium px-6 py-3 rounded-md transition-all duration-300  relative group text-p5  2xl:text-p1">
-            Discover More
-          </button>
-        </div>
-      </div>{" "}
-    </section>
+      </section>
+      <LoginSignupModal
+        isOpen={isLoginModalOpen}
+        setIsOpenLoginSignup={setIsLoginModalOpen}
+        onClose={closeLoginModal}
+      />
+    </>
   );
 };
 
