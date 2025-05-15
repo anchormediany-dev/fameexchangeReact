90fcef36b094a261e37fdb3ec1811ec2fa9e547c
antonzenkov91 <antonzenkov91@gmail.com>
2025-05-15 12:36:17 +0500

    add the footer in talent tokens

--- src/pages/talent_tokens/TalentTokens.jsx
+++ src/pages/talent_tokens/TalentTokens.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
 
 const TalentTokens = () => {
   return (
     <div className="mt-20">
       <Navbar />
+      <Footer />
     </div>
   );
 };
