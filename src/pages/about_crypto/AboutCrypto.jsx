0eb5d10a77e9937df913ecb9f12deaebedde1798
antonzenkov91 <antonzenkov91@gmail.com>
2025-07-01 11:19:18 +0500

    Fix the gerneral issues in pages

--- src/pages/about_crypto/AboutCrypto.jsx
+++ src/pages/about_crypto/AboutCrypto.jsx
@@ -3,7 +3,7 @@ import Footer from "../../components/Footer";
 
 const AboutCrypto = () => {
   return (
-    <div className="mt-20 flex flex-col min-h-screen">
+    <div className="mt-10 lg:mt-16 flex flex-col min-h-screen">
       <Navbar />
 
       <div className="flex-grow py-10 bg-gradient-custom-vertical2 bg-gradient-custom-horizontal2">
