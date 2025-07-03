a4c9f3968e1fcc86adf2f83d7b4528d1b002a9bd
antonzenkov91 <antonzenkov91@gmail.com>
2025-07-03 11:12:25 +0500

    Add the fixes in about us page

--- src/pages/about/AboutUs.jsx
+++ src/pages/about/AboutUs.jsx
@@ -3,16 +3,16 @@ import Footer from "../../components/Footer";
 
 const AboutUs = () => {
   return (
-    <div className="mt-10 lg:mt-16 flex flex-col min-h-screen">
+    <div className=" flex flex-col min-h-screen">
       <Navbar />
 
-      <div className="flex-grow py-10 bg-gradient-custom-vertical2 bg-gradient-custom-horizontal2">
+      <div className="flex-grow mt-10 lg:mt-16 2xl:mt-20 py-12 2xl:py-16 bg-gradient-custom-vertical2 bg-gradient-custom-horizontal2">
         <div className="container flex flex-col gap-8 px-4 sm:px-6 md:px-8 lg:px-12">
           <h3 className="gredient-text max-w-[1407px] text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-center font-bold">
             About Us
           </h3>
 
-          <p className="text-[#5F5F5F] max-w-[1407px] text-sm sm:text-base md:text-lg lg:text-xl leading-relaxed sm:leading-[180%] tracking-wide font-normal text-justify">
+          <p className="text-[#5F5F5F] max-w-3xl mx-auto text-sm sm:text-base md:text-lg lg:text-xl leading-relaxed sm:leading-[180%] tracking-wide font-normal text-justify">
             The Fame Exchange is a groundbreaking social trading platform where
             entertainment, sports, and finance converge. Built for a new
             generation of creators, athletes, influencers, and their audiences,
