20999a645d323acacd8ab78f4c75b4ff4797ab28
antonzenkov91 <114734470+antonzenkov91@users.noreply.github.com>
2025-11-06 15:18:42 +0500

    Fix the issues

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,7 +6,6 @@ import HeroSection from "../../components/HeroSection";
 import BrandedTalentShares from "../../components/BrandedTalentShares";
 import TalentTradingSection from "../../components/TalentTradingSection";
 import DownloadApp from "../../components/DownloadApp";
-import VideoBanner2 from "../../components/VideoBanner2";
 import GigsEvents from "../../components/GigsEvents";
 import MeetAndGreet from "../../components/MeetGreetSection";
 import TheFuturesSection from "../../components/TheFuturesSection";
